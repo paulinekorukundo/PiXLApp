a193d555cbe9c087fd945a9a00b7e8c3485bd27d
pfw_pauline_korukundo <korup01@pfw.edu>
2024-11-03 23:06:12 -0500

    Add Add, Update and Delete Methods in Service and Controller Layers Add Get All Profiles Method in Service and Controller Layer Add checks for Adding existing profile, updating inexistent profile and delete inexistent profile

--- mainframe/src/main/java/com/PiXl/mainframe/services/impl/ProfileServiceImpl.java
+++ mainframe/src/main/java/com/PiXl/mainframe/services/impl/ProfileServiceImpl.java
@@ -13,12 +13,15 @@ import com.PiXl.mainframe.entities.ProfileEntity;
 import com.PiXl.mainframe.models.Profile;
 import com.PiXl.mainframe.services.ProfileService;
 
+import jakarta.validation.Valid;
+
+
 
 @Service
 public class ProfileServiceImpl implements ProfileService {
 
 	@Autowired
-    private ProfileRepository profileRepository;
+    private ProfileRepository pr;
 
     /**
      * Searches for profiles where the first name or last name is contained in the search term.
@@ -28,7 +31,7 @@ public class ProfileServiceImpl implements ProfileService {
      */
 	@Override
     public List<Profile> searchProfilesByName(String name) {
-        lombok.var profileEntities = profileRepository.findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(name, name);
+        lombok.var profileEntities = pr.findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(name, name);
         return from(profileEntities);
     }
 	
@@ -39,11 +42,22 @@ public class ProfileServiceImpl implements ProfileService {
      * @return Optional containing Profile object if found, otherwise empty.
      */
     @Override
-    public Optional<Profile> findByProfileId(Long profileId) {
-        Optional<ProfileEntity> profileEntity = profileRepository.findByProfileId(profileId);
+    public Optional<Profile> getProfile(Long profileId) {
+        Optional<ProfileEntity> profileEntity = pr.findByProfileId(profileId);
         return profileEntity.map(Profile::new);
     }
-
+    
+    
+    /**
+     * Get all profiles
+     * @return a list of all profiles
+     */
+    @Override
+    public List<Profile> getProfiles() {
+    	lombok.var profileEntities = (List<ProfileEntity>) pr.findAll();
+    	return from(profileEntities);
+    }
+    
     /**
      * Finds all profiles that contain a specified food preference.
      *
@@ -52,7 +66,7 @@ public class ProfileServiceImpl implements ProfileService {
      */
     @Override
     public List<Profile> findByFoodPreferencesContaining(String foodPreference) {
-        List<ProfileEntity> profileEntities = profileRepository.findByFoodPreferencesContainingIgnoreCase(foodPreference);
+        List<ProfileEntity> profileEntities = pr.findByFoodPreferencesContainingIgnoreCase(foodPreference);
         return profileEntities.stream().map(Profile::new).collect(Collectors.toList());
     }
 	
@@ -65,7 +79,7 @@ public class ProfileServiceImpl implements ProfileService {
      */
     @Override
     public List<Profile> findByBioContaining(String bio) {
-        List<ProfileEntity> profileEntities = profileRepository.findByBioContainingIgnoreCase(bio);
+        List<ProfileEntity> profileEntities = pr.findByBioContainingIgnoreCase(bio);
         return profileEntities.stream().map(Profile::new).collect(Collectors.toList());
     }
 
@@ -77,9 +91,56 @@ public class ProfileServiceImpl implements ProfileService {
      */
     @Override
     public boolean existsByProfileId(Long profileId) {
-        return profileRepository.existsByProfileId(profileId);
+        return pr.existsByProfileId(profileId);
     }
+    
+    /**
+     * Adds a new profile object
+     * @param profile - the profile to be added 
+     * @return profile - the added profile object
+     */
+	@Override
+	public Profile add(@Valid Profile profile) {
+		Profile addedProfile = null;
+		ProfileEntity profileEntityToAdd = new ProfileEntity(profile);
+		if(!pr.existsByProfileId(profileEntityToAdd.getProfileId())) {
+			lombok.var profileEntityVar = pr.save(profileEntityToAdd);
+			addedProfile = new Profile(profileEntityVar);
+		}
+		return addedProfile;
+	}
 	
+	/**
+	 * Updates an existing profile in the repository
+	 * @param profile - The profile to be updated
+	 * @return profile - the updated profile object
+	 */
+	@Override
+	public Profile update(@Valid Profile profile) {
+		Profile updatedProfile = null;
+		ProfileEntity profileEntityToUpdate = new ProfileEntity(profile);
+		if(pr.existsByProfileId(profileEntityToUpdate.getProfileId())) {
+			lombok.var updatedProfileEntityVar = pr.save(new ProfileEntity(profile));
+			updatedProfile = new Profile(updatedProfileEntityVar);
+		}
+		return updatedProfile;
+	}
+	
+	/**
+	 * Deletes an existing profile from the repository
+	 * @param profile - The profile to be deleted
+	 * @return true - the profile is successfully deleted
+	 */
+	@Override
+	public boolean delete(@Valid Profile profile) {
+		boolean isDeleted = false;
+		ProfileEntity profileEntityToDeletEntity = new ProfileEntity(profile);
+		if(pr.existsByProfileId(profileEntityToDeletEntity.getProfileId())){
+			pr.delete(new ProfileEntity(profile));
+			isDeleted = true;
+		}
+		return isDeleted;
+	}
 	
 	/**
 	 * Convert a list of profile entities to  profile objects
